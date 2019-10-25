f9089ec39e5fb5cc496ed3cf0c4f564db2272b03
m.zorin <m.zorin@MZORIN>
2019-10-25 17:27:29 +0300

    Updated logo icon; added class for restoring last activity of user

---
+++ LLA.GUI/Configuration/LastConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LLA.GUI.Configuration
+{
+    [JsonObject("LastConfiguration")]
+    public class LastConfiguration
+    {
+        [JsonProperty("LastSessionFiles", Order = 1)]
+        public HashSet<String> LastSessionFiles { get; set; }
+
+        public LastConfiguration()
+        {
+            LastSessionFiles = new HashSet<string>();
+        }
+    }
+}
